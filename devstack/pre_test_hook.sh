7452349df2d28aa41f7570f2e8aa32479ecffa6a
Flavio Fernandes <ffernand@redhat.com>
2015-07-23 13:35:27 -0400

    Bug #1484523: Make ODL_NETVIRT_DEBUG_LOGS disabled by default
    This commit changes the default DEBUG logs to be disabled, and have
    it enabled when networking-odl is used as part of devstack gate test.
    This is needed so debug logs don't need to be explicitly disabled in
    non-test deployments of Opendaylight with Openstack.
    
    Change-Id: I6f3cade7d0b86a4ecc637a5e5847d93a46509ed9

--- devstack/pre_test_hook.sh
+++ devstack/pre_test_hook.sh
@@ -5,7 +5,15 @@ set -xe
 # Drop a token that marks the build as coming from openstack infra
 GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
-echo "IS_GATE=True" >> $DEVSTACK_PATH/localrc
+
+cat <<EOF >> $DEVSTACK_PATH/localrc
+
+IS_GATE=True
 
 # Set here the ODL release to use for the Gate job
-echo "ODL_RELEASE=lithium-snapshot-0.3.1" >> $DEVSTACK_PATH/localrc
+ODL_RELEASE=lithium-snapshot-0.3.1
+
+# Enable debug logs for odl ovsdb
+ODL_NETVIRT_DEBUG_LOGS=True
+
+EOF
