3bfc0c8a1399340a0b18c2af317c05fb08943a6a
Federico Ressi <federico.ressi@intel.com>
2015-12-17 05:18:15 +0000

    Correct typo in comment.
    Change-Id: I325b277558c676263cd8fc73f0321cefbb3c9a50

--- devstack/setup_java.sh
+++ devstack/setup_java.sh
@@ -131,7 +131,7 @@ else
 
     function install_openjdk {
         local VERSION="$1"
-        # Can't use yum_install because it woudl exit in case of failure
+        # Can't use yum_install because it would exit in case of failure
         sudo yum install -y java-1.$VERSION.*-openjdk-headless
     }
 
