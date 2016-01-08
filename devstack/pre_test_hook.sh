219f487876a3825f1f835d7ce246c5fb8fdb3de4
Isaku Yamahata <isaku.yamahata@intel.com>
2016-01-08 12:27:48 -0800

    devstack: use odl lithium snapshot 0.3.3 instead of 0.3.1
    This patch make CI gate use lithium snapshot 0.3.3 instead of 0.3.1.
    This is also preparation for adding gate with beryllium snapshot.
    
    Change-Id: Ida9015812568b923ede23272bf36e4784b1d43e0

--- devstack/pre_test_hook.sh
+++ devstack/pre_test_hook.sh
@@ -6,12 +6,26 @@ set -xe
 GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
+case "$ODL_RELEASE_BASE" in
+    beryllium-snapshot)
+        ODL_RELEASE=beryllium-snapshot-0.4.0
+        ;;
+    lithium-snapshot)
+        ODL_RELEASE=lithium-snapshot-0.3.3
+        ;;
+    *)
+        # for compatibility before updating
+        # project-config/jenkins/jobs/networking-odl.yaml
+        ODL_RELEASE=lithium-snapshot-0.3.3
+        ;;
+esac
+
 cat <<EOF >> $DEVSTACK_PATH/localrc
 
 IS_GATE=True
 
 # Set here the ODL release to use for the Gate job
-ODL_RELEASE=lithium-snapshot-0.3.1
+ODL_RELEASE=${ODL_RELEASE}
 
 # Switch to using the ODL's L3 implementation
 ODL_L3=True
