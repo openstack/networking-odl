aeea7ff83bd4c0191bf0ac5ab656fe7f6d599989
Isaku Yamahata <isaku.yamahata@intel.com>
2016-08-08 17:40:30 -0700

    test_pseudo_agentdb_binding.py: adopt neutron_lib
    use neutron_lib for constants instead of neutron.constants
    
    Change-Id: Ibed3b3547d3d66e5bda39c2493891d1e40ef35a3

--- networking_odl/tests/unit/ml2/test_pseudo_agentdb_binding.py
+++ networking_odl/tests/unit/ml2/test_pseudo_agentdb_binding.py
@@ -18,12 +18,12 @@ import mock
 from os import path as os_path
 from string import Template
 
-from neutron.common import constants as n_const
 from neutron.extensions import portbindings
 from neutron.plugins.common import constants
 from neutron.plugins.ml2 import config
 from neutron.plugins.ml2 import driver_api as api
 from neutron.plugins.ml2 import driver_context as ctx
+from neutron_lib import constants as n_const
 
 from networking_odl.ml2 import pseudo_agentdb_binding
 from networking_odl.tests import base
