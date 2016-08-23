74a8c3f0fb914220026e59455355afdc95ac5a2b
Isaku Yamahata <isaku.yamahata@intel.com>
2016-08-23 11:02:41 -0700

    populate json with both 'project_id', 'tenant_id'
    Neutron keyston v3 support is on-going [1].
    From ODL perspective, it's simply variable rename from 'tenant_id'
    to 'project_id'.
    For ODL compatibility, populate json value for ODL neutron northbound
    with both 'project_id' and 'tenant_id'.
    Since ODL Neutron Northbound silently ignores unknown fields (more
    precisely, Java jaxb does), it won't harm to have extra fields unknown
    to ODL neutron northbound.
    
    [1] https://specs.openstack.org/openstack/neutron-specs/specs/newton/moving-to-keystone-v3.html
    
    Change-Id: Id1b18d76871edeab623f03d839616d6729a8dc73
    Closes-Bug: #1616128

---
+++ networking_odl/tests/unit/common/test_filters.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2016 Intel Corp. Isaku Yamahata <isaku.yamahata@gmail com>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.tests import base
+
+from networking_odl.common import filters
+
+
+class TestFilters(base.DietTestCase):
+
+    def _check_id(self, resource, project_id):
+        filters._populate_project_id_and_tenant_id(resource)
+        self.assertIn(resource['project_id'], project_id)
+        self.assertIn(resource['tenant_id'], project_id)
+
+    def _test_populate_project_id_and_tenant_id(self, project_id):
+        self._check_id({'project_id': project_id}, project_id)
+        self._check_id({'tenant_id': project_id}, project_id)
+        self._check_id({'project_id': project_id,
+                        'tenant_id': project_id}, project_id)
+
+    def test_populate_project_id_and_tenant_id_with_id(self):
+        self._test_populate_project_id_and_tenant_id(
+            '01234567-890a-bcde-f012-3456789abcde')
+        self._test_populate_project_id_and_tenant_id("")
+
+    def test_populate_project_id_and_tenant_id_without_id(self):
+        resource = {}
+        filters._populate_project_id_and_tenant_id(resource)
+        self.assertNotIn('project_id', resource)
+        self.assertNotIn('tenant_id', resource)
+
+    def test_populate_project_id_and_tenant_id_with_router(self):
+        # test case for OpenDaylightL3RouterPlugin.delete_router()
+        # it passes data as dependency_list as list, not dict
+        resource0 = ['gw_port_id']
+        resource1 = resource0[:]
+        filters._populate_project_id_and_tenant_id(resource1)
+        self.assertEqual(resource0, resource1)
+
+    def test_populate_project_id_and_tenant_id_with_floatingip(self):
+        # test case for OpenDaylightL3RouterPlugin.delete_floatingip()
+        # it passes data as dependency_list as list, not dict.
+        resource0 = ['router_uuid', 'floatingip_uuid']
+        resource1 = resource0[:]
+        filters._populate_project_id_and_tenant_id(resource1)
+        self.assertEqual(resource0, resource1)
