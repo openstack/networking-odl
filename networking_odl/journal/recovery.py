767e23a5f05fcccee5d739c5463dd52c4762bf5b
Mike Kolesnik <mkolesni@redhat.com>
2016-09-19 09:16:10 +0300

    Journal recovery for syncing with ODL
    Adds the capability for journal recovery to query the resource in the
    DB and determine what to do based on whether the resource exists in the
    DB or not.
    
    Change-Id: Iafed845f965c76c0d90ed30e6c59603bf2354efd
    Partially-implements: blueprint journal-recovery

--- networking_odl/journal/recovery.py
+++ networking_odl/journal/recovery.py
@@ -14,18 +14,25 @@
 #  under the License.
 #
 
+from neutron import context as neutron_context
+from neutron import manager
+from neutron.plugins.common import constants
+from neutron_lib import exceptions as nexc
 from oslo_log import log as logging
 
-from networking_odl._i18n import _LE
+from networking_odl._i18n import _, _LE, _LW
 from networking_odl.common import client
 from networking_odl.common import constants as odl_const
 from networking_odl.db import db
+from networking_odl.journal import journal
 
-_CREATE_OPS = (odl_const.ODL_CREATE, )
-_DELETE_OPS = (odl_const.ODL_DELETE, )
 _CLIENT = client.OpenDaylightRestClientGlobal()
 
 LOG = logging.getLogger(__name__)
+
+
+class UnsupportedResourceType(Exception):
+    pass
 
 
 def journal_recovery(session):
@@ -38,22 +45,58 @@ def journal_recovery(session):
                 _handle_existing_resource(session, row)
             else:
                 _handle_non_existing_resource(session, row)
+        except UnsupportedResourceType:
+            LOG.warning(_LW('Unsupported resource %s'), row.object_type)
         except Exception:
             LOG.exception(
                 _LE("Failure while recovering journal entry %s."), row)
 
 
-def _handle_existing_resource(session, row):
-    if row.operation in _CREATE_OPS:
-        db.update_db_row_state(session, row, odl_const.COMPLETED)
-    elif row.operation in _DELETE_OPS:
-        db.update_db_row_state(session, row, odl_const.PENDING)
+def _get_latest_resource(row):
+    object_type = row.object_type
 
-    # TODO(mkolesni): Handle UPDATE somehow.
+    if object_type in odl_const.L2_RESOURCES:
+        plugin = manager.NeutronManager.get_plugin()
+    elif object_type in odl_const.L3_RESOURCES:
+        plugin = manager.NeutronManager.get_service_plugins().get(
+            constants.L3_ROUTER_NAT)
+    else:
+        raise UnsupportedResourceType(
+            _("unsupported resource type: {}").format(object_type))
+
+    obj_getter = getattr(plugin, 'get_{}'.format(object_type))
+    return obj_getter(neutron_context.get_admin_context(), row.object_uuid)
+
+
+def _sync_resource_to_odl(session, row, operation_type, exists_on_odl):
+    resource = None
+    try:
+        resource = _get_latest_resource(row)
+    except nexc.NotFound:
+        if exists_on_odl:
+            journal.record(
+                neutron_context.get_admin_context(), None, row.object_type,
+                row.object_uuid, odl_const.ODL_DELETE, [])
+    else:
+        journal.record(
+            neutron_context.get_admin_context(), None, row.object_type,
+            row.object_uuid, operation_type, resource)
+
+    db.update_db_row_state(session, row, odl_const.COMPLETED)
+
+
+def _handle_existing_resource(session, row):
+    if row.operation == odl_const.ODL_CREATE:
+        db.update_db_row_state(session, row, odl_const.COMPLETED)
+    elif row.operation == odl_const.ODL_DELETE:
+        db.update_db_row_state(session, row, odl_const.PENDING)
+    else:
+        _sync_resource_to_odl(session, row, odl_const.ODL_UPDATE, True)
 
 
 def _handle_non_existing_resource(session, row):
-    if row.operation in _DELETE_OPS:
+    if row.operation == odl_const.ODL_DELETE:
         db.update_db_row_state(session, row, odl_const.COMPLETED)
-
-    # TODO(mkolesni): Handle other use cases.
+    else:
+        _sync_resource_to_odl(session, row, odl_const.ODL_CREATE, False)
+        # TODO(mkolesni): Handle missing parent resources somehow.
--- networking_odl/tests/unit/journal/test_recovery.py
+++ networking_odl/tests/unit/journal/test_recovery.py
@@ -14,8 +14,12 @@
 #  under the License.
 #
 
+import mock
+
 from neutron.db import api as neutron_db_api
+from neutron import manager
 from neutron.tests.unit.testlib_api import SqlTestCaseLight
+from neutron_lib import exceptions as nexc
 
 from networking_odl.common import constants as odl_const
 from networking_odl.db import db
@@ -38,36 +42,117 @@ class RecoveryTestCase(SqlTestCaseLight):
     def _db_cleanup(self):
         self.db_session.query(models.OpendaylightJournal).delete()
 
+    def _mock_resource(self, plugin, resource_type):
+        mock_resource = mock.MagicMock()
+        get_func = getattr(plugin, 'get_{}'.format(resource_type))
+        get_func.return_value = mock_resource
+        return mock_resource
+
+    def _mock_row(self, resource_type):
+        return mock.MagicMock(object_type=resource_type)
+
+    def _test__get_latest_resource(self, plugin, resource_type):
+        mock_resource = self._mock_resource(plugin, resource_type)
+        mock_row = self._mock_row(resource_type)
+
+        resource = recovery._get_latest_resource(mock_row)
+        self.assertEqual(mock_resource, resource)
+
+    @mock.patch.object(manager.NeutronManager, 'get_plugin')
+    def test__get_latest_resource_l2(self, plugin_mock):
+        for resource_type in odl_const.L2_RESOURCES:
+            plugin = plugin_mock.return_value
+            self._test__get_latest_resource(plugin, resource_type)
+
+    @mock.patch.object(manager.NeutronManager, 'get_service_plugins')
+    def test__get_latest_resource_l3(self, plugin_mock):
+        for resource_type in odl_const.L3_RESOURCES:
+            plugin = plugin_mock.return_value.get.return_value
+            self._test__get_latest_resource(plugin, resource_type)
+
+    def test__get_latest_resource_unsupported(self):
+        mock_row = self._mock_row('aaa')
+        self.assertRaises(
+            recovery.UnsupportedResourceType, recovery._get_latest_resource,
+            mock_row)
+
+    @mock.patch.object(manager.NeutronManager, 'get_plugin')
+    def test__get_latest_resource_none(self, plugin_mock):
+        plugin_mock.return_value.get_network.side_effect = nexc.NotFound()
+
+        mock_row = self._mock_row(odl_const.ODL_NETWORK)
+        self.assertRaises(
+            nexc.NotFound, recovery._get_latest_resource, mock_row)
+
     def test_journal_recovery_no_rows(self):
         recovery.journal_recovery(self.db_session)
         self.assertFalse(self._CLIENT.get_resource.called)
 
-    def _test_journal_recovery(self, operation, odl_resource, expected_state):
+    def _test_recovery(self, operation, odl_resource, expected_state):
         db.create_pending_row(
             self.db_session, odl_const.ODL_NETWORK, 'id', operation, {})
-        row = db.get_all_db_rows(self.db_session)[0]
-        db.update_db_row_state(self.db_session, row, odl_const.FAILED)
+        created_row = db.get_all_db_rows(self.db_session)[0]
+        db.update_db_row_state(self.db_session, created_row, odl_const.FAILED)
 
         self._CLIENT.get_resource.return_value = odl_resource
 
         recovery.journal_recovery(self.db_session)
 
-        row = db.get_all_db_rows(self.db_session)[0]
-        self.assertEqual(expected_state, row['state'])
+        row = db.get_all_db_rows_by_state(self.db_session, expected_state)[0]
+        self.assertEqual(created_row['seqnum'], row['seqnum'])
+        return created_row
 
     def test_journal_recovery_hadles_failure_quietly(self):
         self._CLIENT.get_resource.side_effect = Exception('')
-        self._test_journal_recovery(
+        self._test_recovery(
             odl_const.ODL_DELETE, None, odl_const.FAILED)
 
     def test_journal_recovery_deleted_row_not_in_odl(self):
-        self._test_journal_recovery(
-            odl_const.ODL_DELETE, None, odl_const.COMPLETED)
+        self._test_recovery(odl_const.ODL_DELETE, None, odl_const.COMPLETED)
 
     def test_journal_recovery_created_row_exists_in_odl(self):
-        self._test_journal_recovery(
-            odl_const.ODL_CREATE, {}, odl_const.COMPLETED)
+        self._test_recovery(odl_const.ODL_CREATE, {}, odl_const.COMPLETED)
 
     def test_journal_recovery_deleted_row_exists_in_odl(self):
-        self._test_journal_recovery(
-            odl_const.ODL_DELETE, {}, odl_const.PENDING)
+        self._test_recovery(odl_const.ODL_DELETE, {}, odl_const.PENDING)
+
+    @mock.patch.object(recovery, '_get_latest_resource')
+    def _test_recovery_creates_operation(
+            self, operation, resource, odl_resource, expected_operation,
+            recovery_mock):
+        if resource is not None:
+            recovery_mock.return_value = resource
+        else:
+            recovery_mock.side_effect = nexc.NotFound
+        original_row = self._test_recovery(
+            operation, odl_resource, odl_const.COMPLETED)
+
+        pending_row = db.get_all_db_rows_by_state(
+            self.db_session, odl_const.PENDING)[0]
+        self.assertEqual(expected_operation, pending_row['operation'])
+        self.assertEqual(original_row['object_type'],
+                         pending_row['object_type'])
+        self.assertEqual(original_row['object_uuid'],
+                         pending_row['object_uuid'])
+
+    def test_recovery_created_row_not_in_odl(self):
+        self._test_recovery_creates_operation(
+            odl_const.ODL_CREATE, {}, None, odl_const.ODL_CREATE)
+
+    def test_recovery_updated_row_not_in_odl(self):
+        self._test_recovery_creates_operation(
+            odl_const.ODL_UPDATE, {}, None, odl_const.ODL_CREATE)
+
+    def test_recovery_updated_resource_missing_but_exists_in_odl(self):
+        self._test_recovery_creates_operation(
+            odl_const.ODL_UPDATE, None, {}, odl_const.ODL_DELETE)
+
+    @mock.patch.object(recovery, '_get_latest_resource')
+    def test_recovery_created_resource_missing_and_not_in_odl(self, rmock):
+        rmock.return_value = None
+        self._test_recovery(odl_const.ODL_CREATE, None, odl_const.COMPLETED)
+
+    @mock.patch.object(recovery, '_get_latest_resource')
+    def test_recovery_updated_resource_missing_and_not_in_odl(self, rmock):
+        rmock.return_value = None
+        self._test_recovery(odl_const.ODL_UPDATE, None, odl_const.COMPLETED)
