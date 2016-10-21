644dd518b34ae9ae38a81d5ab50aec0ad6060216
Isaku Yamahata <isaku.yamahata@intel.com>
2016-10-21 10:54:17 -0700

    stop doing any magic cloning of neutron during CI
    Adapted from neutron-lbaas change [1] done by Doug Wiegley.
    [1] Iee43c68b4cc954b9780de02625de7e053ebd27b3
    use constraints for tox targets.
    
    Change-Id: I80eabaf859aabb3a440f4ef2e95a4d779b2286c1
    Depends-On: Ic3422cc42b3970fcc884d2961a041d9e6c3647a8
    Related-bug: #1497897
    Closes-bug: #1634940
    Closes-bug: #1635680
    (cherry picked from commit bd452408a72560254956dc9be3d67af40e0f0666)
    with the adjustment of branch: master -> stable/liberty

---
+++ tools/tox_install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -e
+set -x
+
+DIR=$(dirname $0)
+${DIR}/tox_install_project.sh neutron neutron $*
+${DIR}/tox_install_project.sh neutron-fwaas neutron_fwaas $*
+${DIR}/tox_install_project.sh neutron-lbaas neutron_lbaas $*
+
+CONSTRAINTS_FILE=$1
+shift
+
+install_cmd="pip install"
+if [ $CONSTRAINTS_FILE != "unconstrained" ]; then
+    install_cmd="$install_cmd -c$CONSTRAINTS_FILE"
+fi
+
+$install_cmd -U $*
