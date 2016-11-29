ff0d05ac6b8d8165f8f29747e0ca2b29c252e66e
Isaku Yamahata <isaku.yamahata@intel.com>
2016-11-28 18:41:39 -0800

    mech driver v2: build dict for sg on update
    precommit callback for security group precommit doesn't pass
    complete information. So build it in callback as bug work around.
    precoomit callback should be passed full information.
    Once it's fixed, this bug work around should be removed.
    The original bug report[1] is filed for ODL, but it turned out
    the issue resides in neutron and networking-odl
    
    [1] https://bugs.opendaylight.org/show_bug.cgi?id=7256
    
    Change-Id: I15b6f5321cd3e860de7ba50dd5dde8672259c9ba
    Related-bug: #1546910
    (cherry picked from commit 194da19e84ddabaa2237bd8f971ac331298e3620)

--- networking_odl/ml2/mech_driver_v2.py
+++ networking_odl/ml2/mech_driver_v2.py
@@ -19,6 +19,7 @@ from oslo_log import log as logging
 from neutron.db.models import securitygroup
 from neutron.extensions import multiprovidernet as mpnet
 from neutron.extensions import providernet
+from neutron import manager
 from neutron.plugins.common import constants as p_const
 from neutron.plugins.ml2 import driver_api as api
 
@@ -197,6 +198,22 @@ class OpenDaylightMechanismDriver(api.MechanismDriver):
             elif object_type == odl_const.ODL_SG_RULE:
                 resource_dict = self._make_security_group_rule_dict(
                     resource_dict)
+        # NOTE(yamahata): bug work around
+        # callback for update of security grouop doesn't pass complete
+        # info. So we have to build it. Once the bug is fixed, remove
+        # this bug work around.
+        # https://launchpad.net/bugs/1546910
+        # https://review.openstack.org/#/c/281693/
+        elif (object_type == odl_const.ODL_SG and
+              operation == odl_const.ODL_UPDATE):
+            # NOTE(yamahata): precommit_update is called before updating
+            # values. so context.session.{new, dirty} doesn't include sg
+            # in question. a dictionary with new values needs to be build.
+            core_plugin = manager.NeutronManager.get_plugin()
+            sg = core_plugin._get_security_group(context, res_id)
+            tmp_dict = self._make_security_group_dict(sg)
+            tmp_dict.update(resource_dict)
+            resource_dict = tmp_dict
 
         object_uuid = (resource_dict.get('id')
                        if operation == 'create' else res_id)
