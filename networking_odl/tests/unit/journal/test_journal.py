2b16c6c14d6c222bde460d1be69086305b1e8c70
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2017-03-30 09:50:22 +0000

    Allow user to specify own method to make url
    To enable user customized url's, user can provide his/her own
    method, which takes row as input and can create url. It is
    to support url's for lbaas driver having url's of following
    type(as given in the link).
    
    https://developer.openstack.org/api-ref/networking/v2/?expanded=show-pool-member-details-detail#show-pool-member-details
    
    Change-Id: Id460444d238e2429b089a251a006704f6c980c41
    Closes-Bug: #1677464

---
+++ networking_odl/tests/unit/journal/test_journal.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2017 NEC Corp.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_utils import uuidutils
+
+from networking_odl.common import constants as odl_const
+from networking_odl.db import models
+from networking_odl.journal import journal
+from networking_odl.tests.unit import base_v2
+
+
+class OpenDaylightJournalThreadTest(base_v2.OpenDaylightTestCase):
+    def setUp(self):
+        super(OpenDaylightJournalThreadTest, self).setUp()
+        self.journal = journal.OpenDaylightJournalThread()
+        self.addCleanup(self.cleanup)
+
+    @staticmethod
+    def cleanup():
+        journal.MAKE_URL.clear()
+
+    def test_json_data(self):
+        object_type = 'testobject'
+        data = 'testdata'
+        row = models.OpenDaylightJournal(object_type=object_type,
+                                         object_uuid=uuidutils.generate_uuid(),
+                                         operation=odl_const.ODL_CREATE,
+                                         data=data)
+
+        self.assertEqual("%ss" % object_type, self.journal._json_data(row)[1])
+
+    def test_json_data_customized_url(self):
+        object_type = 'randomtestobject'
+        data = 'testdata'
+        journal.register_url_builder(object_type, lambda row: row.object_type)
+        row = models.OpenDaylightJournal(object_type=object_type,
+                                         object_uuid=uuidutils.generate_uuid(),
+                                         operation=odl_const.ODL_CREATE,
+                                         data=data)
+
+        url_param = self.journal._json_data(row)
+        self.assertEqual(object_type, url_param[1])
