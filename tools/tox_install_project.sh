520fecb22a5d53fd3217072739c0ffb4261603a2
Manjeet Singh Bhatia <manjeet.s.bhatia@intel.com>
2017-09-20 22:09:33 +0000

    Change the cloner branch to stable/pike
    CI for stable/pike is broken because master of
    neutron is clonned for ci jobs.
    
    devstack: use karaf for Nitrogen snapshot
    
    use karaf for Nitrogen snapshot instead of distribution-karaf image
    because distribution-karaf isn't available for Nitrogen at the moment.
    (cherry picked from e347695b78cb82bf94fa073e90ce30fc4aa87c14)
    
    Change-Id: Ia86bb166184bae7576d16c27817a1037255efe45

--- tools/tox_install_project.sh
+++ tools/tox_install_project.sh
@@ -21,7 +21,7 @@ shift 2
 
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
 neutron_installed=$(echo "import ${MOD}" | python 2>/dev/null ; echo $?)
-BRANCH_NAME=master
+BRANCH_NAME=stable/pike
 NEUTRON_DIR=$HOME/${PROJ}
 
 set -e
