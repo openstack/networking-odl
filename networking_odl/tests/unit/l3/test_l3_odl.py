0512a500be161d99d854b77a89066e7179bc5683
Rajiv Kumar <rajiv.kumar@nectechnologies.in>
2017-10-26 09:59:39 +0000

    Revert skip of test l3 test case
    Changes introduced by https://review.openstack.org/#/c/514470/
    broke l3 v1 driver tests therefore those tests were being skipped.
    
    A revert for the same(https://review.openstack.org/#/c/515691/)
    has been merged, now there is no need to skip test cases.
    
    Change-Id: I062ae3f7d74cb83258b4e1e5ce3769b7675d479a

--- networking_odl/tests/unit/l3/test_l3_odl.py
+++ networking_odl/tests/unit/l3/test_l3_odl.py
@@ -19,7 +19,6 @@ Tests for the L3 service plugin for networking-odl.
 import copy
 
 import mock
-import testtools
 
 from neutron.extensions import l3
 from neutron.extensions import l3_ext_gw_mode
@@ -34,8 +33,6 @@ from networking_odl.tests import base as odl_base
 _get_path = test_base._get_path
 
 
-# TODO(mkolesni): Fix if you still care about V1, which is going to be removed
-@testtools.skip("Broken, but we don't care since we're not supporting V1")
 class Testodll3(test_extensions_base.ExtensionTestCase):
 
     fmt = 'json'
